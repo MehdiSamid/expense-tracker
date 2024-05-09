41b0800a3b23861625d6fa7ae7e5e4fce89670ba
KADDARI Ziad <ziadkaddari1@gmail.com>
2024-05-09 16:10:51 +0100

    changingExpensesToReactiveForms

--- src/app/components/add-expense/add-expense.component.ts
+++ src/app/components/add-expense/add-expense.component.ts
@@ -2,52 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { Category } from '../../shared/enums/category';
 import { CommonModule } from '@angular/common';
 import { IExpense } from '../../interfaces/iexpense';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ExpenseService } from '../../services/expense.service';
 
 @Component({
   selector: 'app-add-expense',
   standalone: true,
-  imports: [CommonModule , FormsModule],
+  imports: [CommonModule , ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './add-expense.component.html',
   styleUrl: './add-expense.component.css'
 })
 export class AddExpenseComponent{
-  constructor(private expenseService : ExpenseService ){}
+  myForm! : FormGroup;
+  constructor(private expenseService : ExpenseService, private formBuilder: FormBuilder  ){}
   ngOnInit(): void {
     console.log(this.expenseService.getAllExpenses());
-    // console.log(this.expenseService.getExpenseById(2));
-    // this.expenseService.deleteExpense(10)
-    // this.expenseService.updateExpense({
-    //   id: 4,
-    //   name: '',
-    //   amount: 0,
-    //   date: new Date,
-    //   category: Category.Food,
-    //   user: 0
-    // });
+    this.myForm= this.formBuilder.group({ 
+      id: 0,
+      name: [null,[Validators.required]],
+      amount: [null,[Validators.required]],
+      category: [null,[Validators.required]],
+      date: new Date,
+      user: 0
+    });
   }
   categories = Object.keys(Category).filter(k => typeof Category[k as any] === "number");
-  expense : IExpense = {
-    id: 0,
-    name: '',
-    amount: 0,
-    date: new Date,
-    category: Category.Food,
-    user: 0
-  }
-  Clear(){
-    this.expense= {
-      id: 0,
-      name: '',
-      amount: 0,
-      date: new Date,
-      category: Category.Food,
-      user: 0
-    }
-  }
   addExpense() {
-    this.expenseService.addExpense(this.expense)
-    this.Clear()
+    if (this.myForm.valid){
+      this.expenseService.addExpense(this.myForm.value);
+      this.myForm.reset();
+    }
   }
 }
