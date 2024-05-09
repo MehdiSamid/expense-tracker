e55167446885592a73b813eff057222e218437a8
Abdelhakim OUFKIR <abdelhakimoufkir2000@gmail.com>
2024-05-09 19:16:54 +0100

    completing income ts

--- src/app/components/add-income/add-income.component.ts
+++ src/app/components/add-income/add-income.component.ts
@@ -2,34 +2,57 @@ import { Component } from '@angular/core';
 import { Iincome } from '../../interfaces/iincome';
 import { IncomeService } from '../../services/income.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-income',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './add-income.component.html',
-  styleUrl: './add-income.component.css'
+  styleUrl: './add-income.component.css',
 })
 export class AddIncomeComponent {
+ 
 
   incomes: Iincome[] = [];
+  myForm!: FormGroup;
 
-  constructor(private incomeService: IncomeService, private router:Router) {}
+  constructor(
+    private incomeService: IncomeService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
-    this.incomes = this.incomeService.getIncomes();
-    console.log(this.incomes);
+    console.log(this.incomeService.getIncomes());
+    this.myForm = this.formBuilder.group({
+      id: 0,
+      name: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: new Date(),
+      userId: 0,
+    });
   }
 
   remove(indexRemove: number) {
     console.log(indexRemove);
     this.incomeService.deleteIncome(indexRemove);
-    }
-
-  edit(indexUpdate: number) {
-    this.router.navigate(["updateArticle/"+indexUpdate])
   }
 
+  edit(indexUpdate: number) {
+    // this.router.navigate(["updateArticle/"+indexUpdate])
+  }
 
-
+  add() {
+    if (this.myForm.valid) {
+      this.incomeService.addIncome(this.myForm.value);
+      this.myForm.reset();
+    }
+  }
 }
