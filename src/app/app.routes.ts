fe5cf7b03baa8fc8ec09d64a77acbf3412c0e6c8
MehdiSamid <96839827+MehdiSamid@users.noreply.github.com>
2024-05-10 10:51:48 +0000

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,21 @@ import { AddExpenseComponent } from './components/add-expense/add-expense.compon
 import { AddIncomeComponent } from './components/add-income/add-income.component';
 import { UpdateExpenseComponent } from './components/update-expense/update-expense.component';
 import { UpdateIncomeComponent } from './components/update-income/update-income.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-export const routes: Routes = [
-    { path:"addexpense",component: AddExpenseComponent},
-    { path:"addincome",component: AddIncomeComponent},
-    { path:"updateexpense/:id",component:UpdateExpenseComponent},
-    { path:"updateincome/:id",component:UpdateIncomeComponent},
-    { path: "**", redirectTo: ""}
+
+  export const routes: Routes = [
+  { path:"addexpense",component: AddExpenseComponent},
+  { path:"addincome",component: AddIncomeComponent},
+  { path:"updateexpense/:id",component:UpdateExpenseComponent},
+  { path:"updateincome/:id",component:UpdateIncomeComponent},
+  { path: "**", redirectTo: ""},
+  {path:"login",component: LoginComponent},
+  {path:"",component: LoginComponent},
+  {path:"signup",component: SignupComponent},
+  {path:"test",component: AddExpenseComponent},
+  {path:"test2",component: AddIncomeComponent},
+
+
 ];
