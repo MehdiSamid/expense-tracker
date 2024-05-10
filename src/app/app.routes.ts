af04f7e475c5759f2a778cfeebc64336f1e1c660
KADDARI Ziad <ziadkaddari1@gmail.com>
2024-05-10 10:12:53 +0100

    changing routes adding comps

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { AddExpenseComponent } from './components/add-expense/add-expense.component';
 import { AddIncomeComponent } from './components/add-income/add-income.component';
+import { UpdateExpenseComponent } from './components/update-expense/update-expense.component';
+import { UpdateIncomeComponent } from './components/update-income/update-income.component';
 
 export const routes: Routes = [
-    {path:"test",component: AddExpenseComponent},
-    {path:"test2",component: AddIncomeComponent},
+    { path:"addexpense",component: AddExpenseComponent},
+    { path:"addincome",component: AddIncomeComponent},
+    { path:"updateexpense/:id",component:UpdateExpenseComponent},
+    { path:"updateincome/:id",component:UpdateIncomeComponent},
+    { path: "**", redirectTo: ""}
 ];
